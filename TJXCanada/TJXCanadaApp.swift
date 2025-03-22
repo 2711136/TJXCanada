76f53c3c708fe979e3386fb545cd61e238624e01
2711136 <loumbamba375@gmail.com>
2025-03-22 14:26:58 -0400

    Initial Commit

---
+++ TJXCanada/TJXCanadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TJXCanadaApp.swift
+//  TJXCanada
+//
+//  Created by Bamba Loum on 2025-03-22.
+//
+
+import SwiftUI
+
+@main
+struct TJXCanadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TJXCanadaTests/TJXCanadaTests.swift
@@ -0,0 +1,16 @@
+//
+//  TJXCanadaTests.swift
+//  TJXCanadaTests
+//
+//  Created by Bamba Loum on 2025-03-22.
+//
+
+import Testing
+
+struct TJXCanadaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
